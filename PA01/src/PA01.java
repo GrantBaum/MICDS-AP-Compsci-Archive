a113582e5200354f157c65dee3a3315e426941fb
Grant Baumstark <gbaumstark@micds.org>
2025-09-12 14:26:50 -0500

    Add files via upload

---
+++ PA01/src/PA01.java
@@ -0,0 +1,18 @@
+
+/*
+Name: Grant Baumstark
+Block: F
+Program: PA01
+Date: 8/20/25
+*/
+
+
+public class PA01 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		System.out.println("This is PA01.");
+
+	}
+
+}
